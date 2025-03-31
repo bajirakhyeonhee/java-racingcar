19b4a5f65450d92c25bf1defdce67c05880f5592
lhh94 <lhh9498@gmail.com>
2025-03-31 20:43:03 +0900

    Car Class (자동차 객체의 이름, 이동을 캡슐화한 클래스)

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,24 +2,34 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    protected String Name;
-    protected String Distance;
+    private static final int MOVE_THRESHOLD = 4; // 피드백: 4를 의미있는 상수로
+    protected String name;
+    private String distance;
+
     public Car(String Name) {
         if (Name == null || Name.length() > 5)
             throw new IllegalArgumentException();
-        this.Name = Name;
-        this.Distance = "";
+        this.name = Name;
+        this.distance = "";
     }
 
-    void move(){
-        int num = Randoms.pickNumberInRange(0,9);
-        if(num>4){
-            for (int i=0; i<num; i++){
-                this.Distance+="-";
-            }
+    public void move() { // 피드백: 접근제어자 명시 (default → public)
+        int num = Randoms.pickNumberInRange(0, 9);
+        if (num >= MOVE_THRESHOLD) {
+            this.distance += "-";
         }
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public String getDistance() {
+        return distance;
+    }
+
+    public int getDistanceLength() {
+        return distance.length();
     }
 
 }
