9851b365b4ebf2726016a4e9cc1af23720af6103
lhh94 <lhh9498@gmail.com>
2025-03-31 20:41:39 +0900

    실행 main(사용자 입력 받고 경주 실행하는 클래스)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,8 @@
 package racingcar;
-import javax.lang.model.element.Name;
-import java.util.Scanner;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -10,15 +11,19 @@ public class Application {
         ArrayList<Car> cars = new ArrayList<>();
         System.out.println("경주할 자동차 이름을 입력하세요.");
         String name = readLine();
-        String[] Name = name.split(",");
-        for(String n: Name){
-            Car car= new Car(n);
+
+        // 피드백: 배열 → List<String>으로 바꿈
+        List<String> nameList = Arrays.asList(name.split(","));
+
+        for (String n : nameList) { // 피드백: 배열 → List<String>으로 바꿈
+            Car car = new Car(n);
             cars.add(car);
         }
-        System.out.println("시도할 회수는 몇회인가요?");
-        int Count = Integer.parseInt(readLine());
 
-        racing race = new racing();
-        race.racing(cars, Count);
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = Integer.parseInt(readLine());
+
+        Racing race = new Racing();
+        race.startRace(cars, count);
     }
 }
